df463ff1ef68df120da5089dc07fca52fac34c82
danny <tnd.290800@gmail.com>
2023-10-04 21:54:09 +0700

    init

---
+++ Controller/EmailControler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using GiaiPhapLogistics.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GiaiPhapLogistics.Controller
+{
+    [Route("api/SendMail")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpPost("test")]
+        public string SendEmail([FromBody] ContactInfo contactInfo)
+        {
+            string fromMail = "giaiphaplogisticemailservice@gmail.com";
+            string fromPassword = "dgbdrrqrftrrykut";
+
+            MailMessage message = new MailMessage();
+            message.From = new MailAddress(fromMail);
+            message.Subject = "Khách hàng liên hệ";
+            message.To.Add(new MailAddress("tnd.290800@gmail.com"));
+            message.Body = "<html> " +
+                "<body> " +
+                    "<h3>Khách hàng liên hệ</h3>" +
+                    "<p>Email: " + contactInfo.Email + "</p>" +
+                    "<p>Tên KH: " + contactInfo.Name + "</p>" +
+                    "<p>Số điện thoại: " + contactInfo.PhoneNumber + "</p>" +
+                    "<p>Lời nhắn: " + contactInfo.Message + "</p>" +
+                "<body> " +
+            "</html>";
+            message.IsBodyHtml = true;
+
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(fromMail, fromPassword),
+                EnableSsl = true,
+            };
+
+            smtpClient.Send(message);
+            return "Send mail success";
+        }
+    }
+}
+
---
+++ Models/ContactInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace GiaiPhapLogistics.Models
+{
+	public class ContactInfo
+	{
+		public string Email { get; set; }
+		public string Name { get; set;}
+		public string PhoneNumber { get; set; }
+		public string Message { get; set; }
+	}
+}
+
